28f52827e2f42dacd174e9ae219ff6dacc9bf515
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-11-01 23:40:32 -0300

    feat: Criação do Cabeçalho

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, eveniet.</h1>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+      <header className="container-header">
+        <nav>
+            <h1>Task<strong>System</strong></h1>
+            <a href="#">Suas Tasks</a>
+        </nav>
+        
+        <a href="#">Logout</a>
+      </header>
+    );
+  }
